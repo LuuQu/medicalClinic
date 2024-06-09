4a5561329d75f53ccd0a4e58cadbf6f91c638555
Łukasz Wojewnik <101529530+LuuQu@users.noreply.github.com>
2024-06-09 10:57:18 +0200

    Swagger

--- src/main/java/com/LuuQu/medicalclinic/controller/AppointmentController.java
+++ src/main/java/com/LuuQu/medicalclinic/controller/AppointmentController.java
@@ -2,21 +2,24 @@ package com.LuuQu.medicalclinic.controller;
 
 import com.LuuQu.medicalclinic.model.dto.AppointmentDto;
 import com.LuuQu.medicalclinic.service.AppointmentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/appointments")
+@RequestMapping(value = "/appointments", produces = "application/json")
 public class AppointmentController {
     private final AppointmentService appointmentService;
 
+    @Operation(summary = "Connect appointment with patient by patient id and appointment id.")
     @PatchMapping("/{appointmentId}/patients/{patientId}")
     public AppointmentDto addPatient(@PathVariable Long patientId, @PathVariable Long appointmentId) {
         return appointmentService.addPatient(patientId, appointmentId);
     }
 
+    @Operation(summary = "Create new appointment by data in request body and connect appointment with doctor by doctor id.")
     @PostMapping("/{doctorId}")
     @ResponseStatus(HttpStatus.CREATED)
     public AppointmentDto addAppointment(@PathVariable Long doctorId, @RequestBody AppointmentDto appointmentDto) {
--- src/main/java/com/LuuQu/medicalclinic/controller/PatientController.java
+++ src/main/java/com/LuuQu/medicalclinic/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.LuuQu.medicalclinic.controller;
 
 import com.LuuQu.medicalclinic.model.dto.PatientDto;
 import com.LuuQu.medicalclinic.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -11,37 +12,43 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/patients")
+@RequestMapping(value = "/patients", produces = "application/json")
 public class PatientController {
     private final PatientService patientService;
 
+    @Operation(summary = "Get list of patients by pages. \"page\": num of page, \"size\": num of elements in page.")
     @GetMapping
     public List<PatientDto> getPatientList(Pageable pageable) {
         return patientService.getPatients(pageable);
     }
 
+    @Operation(summary = "Get single patient by id.")
     @GetMapping("/{id}")
     public PatientDto getPatient(@PathVariable Long id) {
         return patientService.getPatient(id);
     }
 
+    @Operation(summary = "Add patient to database. Patient description in request body.")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PatientDto addPatient(@RequestBody PatientDto patient) {
         return patientService.addPatient(patient);
     }
 
+    @Operation(summary = "Delete patient by id.")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deletePatient(@PathVariable Long id) {
         patientService.deletePatient(id);
     }
 
+    @Operation(summary = "Edit patient with given id and replace data by request body.")
     @PutMapping("/{id}")
     public PatientDto editPatient(@PathVariable Long id, @RequestBody PatientDto patient) {
         return patientService.editPatient(id, patient);
     }
 
+    @Operation(summary = "Edit password in request body to patient with given id.")
     @PatchMapping("/{id}/password")
     public PatientDto editPassword(@PathVariable Long id, @RequestBody PatientDto patient) {
         return patientService.editPassword(id, patient);
